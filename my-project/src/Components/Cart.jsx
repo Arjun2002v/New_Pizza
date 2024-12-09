08423d1c80f74cfa6c0838dea3a1c3c25c0606a3
arjun vapalla <arjun.v@nxtgen.com>
2024-12-09 17:56:47 +0530

    added style to the cart page

--- my-project/src/Components/Cart.jsx
+++ my-project/src/Components/Cart.jsx
@@ -7,18 +7,69 @@ const Cart = () => {
   const [activeData] = useAtom(activeAtom);
 
   return (
-    <div>
-      <h1>Cart</h1>
-      <h3>Total Quantity: {totalQuantity}</h3>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "45px",
+      }}
+    >
+      <h1
+        style={{ fontFamily: "Gabarito", fontWeight: "700", fontSize: "30px" }}
+      >
+        Cart
+      </h1>
+      <h3 style={{ fontFamily: "Gabarito", fontWeight: "700" }}>
+        Total Quantity: {totalQuantity}
+      </h3>
       <div>
-        {activeData.map((item) => (
-          <div key={item.id}>
-            <h4>{item.name}</h4>
-            <p>Quantity: {item.quantity}</p>
-            <p>Price: Rs. {item.price / 100 || item.defaultPrice / 100}</p>
-          </div>
-        ))}
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+
+            gap: "20px",
+          }}
+        >
+          {activeData.map((item) => (
+            <div
+              key={item.id}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "60px",
+                alignItems: "center",
+                border: "none",
+                padding: "15px",
+                borderRadius: "10px",
+                marginBottom: "10px",
+                background: "grey",
+                color: "white",
+                fontFamily: "Gabarito",
+                fontWeight: "700",
+              }}
+            >
+              <h4
+                style={{
+                  fontFamily: "Gabarito",
+                  fontWeight: "700",
+                  color: "white",
+                }}
+              >
+                {item.name}
+              </h4>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <p>Quantity: {item.quantity}</p>
+                <p>Price: Rs. {item.price / 100 || item.defaultPrice / 100}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      <h3 style={{ fontFamily: "Gabarito", fontWeight: "300" }}>
+        Total Price to be Paid Rs:{" "}
+      </h3>
     </div>
   );
 };
